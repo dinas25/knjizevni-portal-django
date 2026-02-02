c7c5a90e263c2affe1605efa5bdfcbeffaa33f1e
Mia <miasnajder.s@gmail.com>
2026-02-02 17:43:23 +0100

    Ažuriranje pogleda i urlova

--- BookReview/urls.py
+++ BookReview/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path, include
 from . import views
-from .views import SignUpView, RecenzijaDeleteView 
 
 urlpatterns = [
     path('', views.DjeloListView.as_view(), name='djelo-list'),
     path('djelo/<int:pk>/', views.DjeloDetailView.as_view(), name='djelo-detail'),
+
+    path('djelo/dodaj/', views.DjeloCreateView.as_view(), name='djelo-create'),
+    path('djelo/<int:pk>/uredi/', views.DjeloUpdateView.as_view(), name='djelo-update'),
+    path('djelo/<int:pk>/obrisi/', views.DjeloDeleteView.as_view(), name='djelo-delete'),
+
+    path('djelo/<int:pk>/recenzija/dodaj/', views.RecenzijaCreateView.as_view(), name='recenzija-create'),
+    path('recenzija/<int:pk>/obrisi/', views.RecenzijaDeleteView.as_view(), name='obrisi-recenziju'),
+
     path('autori/', views.AutorListView.as_view(), name='autor-list'),
+    path('autori/<int:pk>/', views.AutorDetailView.as_view(), name='autor-detail'),
+
     path('recenzije/', views.RecenzijaListView.as_view(), name='recenzija-list'),
-    path("accounts/", include("django.contrib.auth.urls")), 
-    path("accounts/signup/", SignUpView.as_view(), name="register"), 
-    path('recenzija/<int:pk>/obrisi/', RecenzijaDeleteView.as_view(), name='obrisi-recenziju'),
+
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/signup/", views.SignUpView.as_view(), name="register"),
 ]
--- BookReview/views.py
+++ BookReview/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, DeleteView
-from .models import Djelo, Autor, Recenzija
+from django.views.generic import (
+    ListView, DetailView, CreateView, DeleteView, UpdateView
+)
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from .models import Djelo, Autor, Recenzija
+from .forms import DjeloForm, RecenzijaForm
+
 
 class DjeloListView(ListView):
     model = Djelo
@@ -16,9 +21,11 @@ class DjeloListView(ListView):
             return Djelo.objects.filter(naslov__icontains=query)
         return Djelo.objects.all()
 
+
 class DjeloDetailView(DetailView):
     model = Djelo
     template_name = "knjige/djelo_detail.html"
+
 
 class AutorListView(ListView):
     model = Autor
@@ -28,32 +35,90 @@ class AutorListView(ListView):
     def get_queryset(self):
         query = self.request.GET.get('q', '')
         if query:
-            return Autor.objects.filter(ime__icontains=query) | Autor.objects.filter(prezime__icontains=query)
+            return (
+                Autor.objects.filter(ime__icontains=query) |
+                Autor.objects.filter(prezime__icontains=query)
+            )
         return Autor.objects.all()
+
 
 class AutorDetailView(DetailView):
     model = Autor
     template_name = "knjige/autor_detail.html"
     context_object_name = "autor"
 
+
 class RecenzijaListView(ListView):
     model = Recenzija
     template_name = "knjige/recenzija_list.html"
     context_object_name = "recenzije"
-    ordering = ['-datum'] 
+    ordering = ['-datum']
+
+
+class DjeloCreateView(LoginRequiredMixin, CreateView):
+    model = Djelo
+    form_class = DjeloForm
+    template_name = "knjige/djelo_form.html"
+    success_url = reverse_lazy("djelo-list")
+
+    def form_valid(self, form):
+        form.instance.dodao = self.request.user
+        return super().form_valid(form)
+
+
+class DjeloUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Djelo
+    form_class = DjeloForm
+    template_name = "knjige/djelo_form.html"
+
+    def get_success_url(self):
+        return reverse_lazy("djelo-detail", kwargs={"pk": self.object.pk})
+
+    def test_func(self):
+        return self.request.user == self.get_object().dodao
+
+
+class DjeloDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Djelo
+    template_name = "knjige/djelo_confirm_delete.html"
+    success_url = reverse_lazy("djelo-list")
+
+    def test_func(self):
+        return self.request.user == self.get_object().dodao
+
+
+class RecenzijaCreateView(LoginRequiredMixin, CreateView):
+    model = Recenzija
+    form_class = RecenzijaForm
+    template_name = "knjige/recenzija_form.html"
+
+    def form_valid(self, form):
+        form.instance.korisnik = self.request.user
+        form.instance.djelo = Djelo.objects.get(pk=self.kwargs['pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "djelo-detail",
+            kwargs={"pk": self.kwargs['pk']}
+        )
+
+
+class RecenzijaDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Recenzija
+    template_name = "registration/recenzija_confirm_delete.html"
+
+    def get_success_url(self):
+        return reverse_lazy(
+            "djelo-detail",
+            kwargs={"pk": self.object.djelo.id}
+        )
+
+    def test_func(self):
+        return self.get_object().korisnik == self.request.user
+
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/register.html"
-
-class RecenzijaDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Recenzija
-    template_name = "registration/recenzija_confirm_delete.html"
-    
-    def get_success_url(self):
-        return reverse_lazy('djelo-detail', kwargs={'pk': self.object.djelo.id})
-
-    def test_func(self):
-        obj = self.get_object()
-        return obj.korisnik == self.request.user
